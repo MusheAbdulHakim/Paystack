d5952034889125366f0d48105f6a626377ffdd55
musheabdulhakim <musheabdulhakim99@gmail.com>
2025-01-02 19:16:31 +0000

    api resource methods added

--- src/Client.php
+++ src/Client.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 
 namespace MusheAbdulHakim\Paystack;
 
+use MusheAbdulHakim\Paystack\Api\Customer;
+use MusheAbdulHakim\Paystack\Api\Terminal;
+use MusheAbdulHakim\Paystack\Api\Transaction;
+use MusheAbdulHakim\Paystack\Api\TransactionSplit;
 use MusheAbdulHakim\Paystack\Contracts\PaystackClientInterface;
 use MusheAbdulHakim\Paystack\Contracts\TransporterContract;
 
@@ -13,9 +17,29 @@ final readonly class Client implements PaystackClientInterface
     /**
      * Creates a Client instance with the given API token.
      */
-    public function __construct()
+    public function __construct(private TransporterContract $transporter)
     {
         // ..
+    }
+
+    public function transaction(): Transaction
+    {
+        return new Transaction($this->transporter);
+    }
+
+    public function transactionSplit(): TransactionSplit
+    {
+        return new TransactionSplit($this->transporter);
+    }
+
+    public function terminal(): Terminal
+    {
+        return new Terminal($this->transporter);
+    }
+
+    public function customer(): Customer
+    {
+        return new Customer($this->transporter);
     }
 
 
