92489dbf6c7c7c0cfc987a9ae54cfac6b3e55347
musheabdulhakim <musheabdulhakim99@gmail.com>
2025-01-03 22:50:41 +0000

    init

---
+++ src/Api/Subscription.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MusheAbdulHakim\Paystack\Api;
+
+use MusheAbdulHakim\Paystack\Api\Concerns\Transportable;
+use MusheAbdulHakim\Paystack\ValueObjects\Transporter\Payload;
+use MusheAbdulHakim\Paystack\Contracts\Api\SubscriptionContract;
+
+final class Subscription implements SubscriptionContract
+{
+    use Transportable;
+
+    public function create(array $params = []): array|string
+    {
+        $payload = Payload::post("subscription", $params);
+        return $this->transporter->requestObject($payload)->data();
+    }
+
+    public function list(array $params = []): array|string
+    {
+        $payload = Payload::get("subscription", $params);
+        return $this->transporter->requestObject($payload)->data();
+    }
+
+    public function fetch(string $id): array|string
+    {
+        $payload = Payload::get("subscription/$id");
+        return $this->transporter->requestObject($payload)->data();
+    }
+
+    public function enable(string $code, string $token): array|string
+    {
+        $params['code'] = $code;
+        $params['token'] = $token;
+        $payload = Payload::post("subscription/enable", $params);
+        return $this->transporter->requestObject($payload)->data();
+    }
+
+    public function disable(string $code, string $token): array|string
+    {
+        $params['code'] = $code;
+        $params['token'] = $token;
+        $payload = Payload::post("subscription/disable", $params);
+        return $this->transporter->requestObject($payload)->data();
+    }
+
+    public function generateLink(string $code): array|string
+    {
+        $payload = Payload::get("subscription/$code/manage/link");
+        return $this->transporter->requestObject($payload)->data();
+    }
+
+    public function sendLink(string $code): array|string
+    {
+        $payload = Payload::post("subscription/$code/manage/email");
+        return $this->transporter->requestObject($payload)->data();
+    }
+}
